1b8a7292351f8c705f775ec58ae9819d06d4dac3
MorskoySom <Somovvitvit@gmail.com>
2023-12-20 15:20:16 +0200

    b1

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 import app from "./app.js";
 
+// xyTonLUkvRKvGWDp
+// before hw3
+// now it is branch hw3
+
 app.listen(3000, () => {
   console.log("Server running. Use our API on port: 3000")
 })
