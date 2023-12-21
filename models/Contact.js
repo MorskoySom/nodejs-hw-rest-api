81c6e3872f64d2c25baa18d30a526dfa55db9a54
MorskoySom <Somovvitvit@gmail.com>
2023-12-21 10:36:06 +0200

    b7

--- models/Contact.js
+++ models/Contact.js
@@ -1,9 +1,21 @@
 import { Schema, model } from "mongoose";
 
-const contactSchema = new Schema({
-    name: String,
-    email: String,
-    phone: String,
+const contactSchema = new Schema({    
+    name: {
+      type: String,
+      required: [true, 'Set name for contact'],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  
 })
 
 const Contact = model("contact", contactSchema);
